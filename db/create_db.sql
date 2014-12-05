54fc50efd343b89b255bd1d58d8fdd4770d4996d
上海宁卫信息技术有限公司 <lihao@nway.com.cn>
2014-12-05 15:00:58 +0800

    Create create_db.sql
    Initialize postgresql database

---
+++ db/create_db.sql
@@ -0,0 +1,14 @@
+--postgresql sqlfile
+-- Database: nwaycc
+
+-- DROP DATABASE nwaycc;
+
+CREATE DATABASE nwaycc
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'zh_CN.utf8'
+       LC_CTYPE = 'zh_CN.utf8'
+       CONNECTION LIMIT = -1;
+       
+       
